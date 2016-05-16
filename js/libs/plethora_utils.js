cffdfcfb83a04328fecf7b267b9b4b29046e0d52
kounelios13 <pkounelios@gmail.com>
2016-05-16 22:21:02 +0300

    fixed browser detection!

--- js/libs/plethora_utils.js
+++ js/libs/plethora_utils.js
@@ -58,11 +58,29 @@ _p.clog = function( msg ){
   var args          = [].slice.apply(arguments);
   //Check if browser supports CSS console
   //Internet explorer does not support it.
-  //Firefox does not support it
-  //Use navigator.vendor to detect browser
-  //Because chrome returns Netscape as vendor name but "Coogle Inc." as navigator.vendor
-  isBrowserSupported=navigator.appName != "Netscape" || navigator.vendor != "";
+  //Firefox  supports it
+  //Safari 5 does not support CSS console
+   var supported_browsers=["Chrome","Firefox"];
+
+  navigator.getBrowserName= (function(){
+      var ua= navigator.userAgent, tem,
+      M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+      if(/trident/i.test(M[1])){
+          tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+          return 'IE '+(tem[1] || '');
+      }
+      if(M[1]=== 'Chrome'){
+          tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+          if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+      }
+      M= M[2]? [M[1]/*, M[2]*/]: [navigator.appName/*, navigator.appVersion*/, '-?'];
+      if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+      return M.join(' ');
+  })();
+  var current_browser=navigator.getBrowserName;
+  var isBrowserSupported = supported_browsers.indexOf(current_browser) != -1;
   if(isBrowserSupported){
+    //Current browser supports what we want to do
     var tag= "clog";
     var currentScript = "";
     if ( document.currentScript ){
@@ -82,7 +100,7 @@ _p.clog = function( msg ){
   }
   else{
     //NOTE:Internet explorer does not support console.warning
-    console.log("Console CSS is not supported in this browser.");
+     console.log("Console CSS is not supported in this browser.");
      console.log.apply( console, args );
   }
 
